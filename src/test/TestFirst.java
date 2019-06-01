f19beef285abf2376445b0e16f2aa9ddfe2a2f8d
hong <hong@192.168.2.1>
2019-06-01 17:07:13 +0800

    update

--- src/test/TestFirst.java
+++ src/test/TestFirst.java
@@ -3,6 +3,8 @@ package test;
 public class TestFirst {
 	public static void main(String[] args) {
 		System.out.println("This my first git project!");
+		
+		System.out.println("This first update!");
 	}
 
 }
